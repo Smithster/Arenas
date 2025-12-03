7fdfdc3fee20f3bacd375c5ada66dca8286bd0e6
smithster <samsmith99@btinternet.com>
2025-12-03 19:35:34 +0000

    chore: prevent errors when punching air

--- src/main/java/uk/smithster/arenas/handlers/useLoadoutTools.java
+++ src/main/java/uk/smithster/arenas/handlers/useLoadoutTools.java
@@ -11,6 +11,10 @@ public class useLoadoutTools implements Listener {
     @EventHandler
     public void onInteraction(PlayerInteractEvent event) {
 
+        if (event.getClickedBlock() == null) {
+            return;
+        }
+
         LoadoutSelect ls = LoadoutSelect.LoadoutSelects.get(event.getClickedBlock().getLocation());
 
         if (ls == null) {
